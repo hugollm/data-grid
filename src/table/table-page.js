423e595e31ce149b28da8d5def0b2416b66d7948
Hugo Mota <hugo.txt@gmail.com>
2017-03-11 13:12:45 -0300

    Add pagination to table page prototype

--- src/table/table-page.js
+++ src/table/table-page.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import Component from 'app/component';
+import './style.scss';
 
 
 export default class TablePage extends Component {
 
     render() {
         return <div className="table-page">
+            <nav aria-label="Page navigation">
+                <ul className="pagination">
+                    <li>
+                        <a href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+                    <li><a href="#">1 / 321</a></li>
+                    <li>
+                        <a href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
             <table className="table table-bordered table-striped table-responsive">
                 <thead>
                     <tr>
