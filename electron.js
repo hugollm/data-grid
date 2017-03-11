13418551af93e71a12cd90bbc80f2fa6b4bf36fc
Hugo Mota <hugo.txt@gmail.com>
2017-03-11 17:07:48 -0300

    Implement connection parameters check in new connection page

--- electron.js
+++ electron.js
@@ -2,9 +2,23 @@ const electron = require('electron');
 const path = require('path');
 
 electron.app.on('ready', start);
+electron.ipcMain.on('test-connection-request', testConnection);
 
 function start() {
     var mainWindow = new electron.BrowserWindow({width: 1280, height: 768});
     mainWindow.setMenuBarVisibility(false);
     mainWindow.loadURL('file://' + path.join(__dirname, 'build/index.html'));
+    // mainWindow.webContents.openDevTools();
+}
+
+function testConnection(event, params) {
+    var pg = require('pg');
+    var client = new pg.Client(params);
+    client.connect((error) => {
+        var valid = !error;
+        var errorMessage = error ? error.message : '';
+        event.sender.send('test-connection-response', {valid: valid, error: errorMessage});
+        if (valid)
+            client.end();
+    });
 }
--- src/connections/add-connection-page.js
+++ src/connections/add-connection-page.js
@@ -1,58 +1,56 @@
 import React from 'react';
-import Component from 'app/component';
+import { ipcRenderer } from 'electron';
 
+import Component from 'app/component';
 import { changePage } from 'app/actions';
 
 
 export default class AddConnectionPage extends Component {
 
-    componentDidMount() {
-        this.refs.host.focus();
+    constructor() {
+        super();
+        this.state = {
+            host: '/var/run/postgresql',
+            port: '5432',
+            database: '',
+            user: 'postgres',
+            password: '',
+            connectionIsValid: null,
+            connectionError: '',
+        };
     }
 
-    render() {
-        return <div className="container add-connection-page">
-            <div className="page-header">
-                <h3>Add connection</h3>
-            </div>
-            <form>
-                <div className="form-group">
-                    <div className="row">
-                        <div className="col-xs-8">
-                            <label>Host</label>
-                            <input type="text" className="form-control" ref="host"/>
-                        </div>
-                        <div className="col-xs-4">
-                            <label>Port</label>
-                            <input type="text" className="form-control"/>
-                        </div>
-                    </div>
-                </div>
-                <div className="form-group">
-                    <label>Database</label>
-                    <input type="text" className="form-control"/>
-                </div>
-                <div className="form-group">
-                    <div className="row">
-                        <div className="col-xs-6">
-                            <label>User</label>
-                            <input type="text" className="form-control"/>
-                        </div>
-                        <div className="col-xs-6">
-                            <label>Password</label>
-                            <input type="text" className="form-control"/>
-                        </div>
-                    </div>
-                </div>
-                <hr/>
-                <div className="form-group">
-                    <button className="btn btn-default">Check connection</button>
-                </div>
-            </form>
-            <hr/>
-            <button className="btn btn-primary disabled" onClick={this.onClickSave.bind(this)}>Save</button>
-            <button className="btn btn-default" onClick={this.onClickCancel.bind(this)}>Cancel</button>
-        </div>;
+    componentDidMount() {
+        this.refs.host.focus();
+        ipcRenderer.on('test-connection-response', (event, status) => {
+            console.log(status);
+            this.setState({connectionIsValid: status.valid, connectionError: status.error});
+        });
+    }
+
+    bindValue(e) {
+        var state = this.state;
+        state[e.target.name] = e.target.value;
+        this.setState(state);
+    }
+
+    isFormValid() {
+        var host = this.state.host.trim();
+        var port = this.state.port.trim();
+        var database = this.state.database.trim();
+        var user = this.state.user.trim();
+        return host && port && database && user;
+    }
+
+    onClickCheckConnection() {
+        var params = {
+            host: this.state.host.trim(),
+            port: this.state.port.trim(),
+            database: this.state.database.trim(),
+            user: this.state.user.trim(),
+            password: this.state.password,
+        };
+        ipcRenderer.send('test-connection-request', params);
     }
 
     onClickSave() {
@@ -61,5 +59,69 @@ export default class AddConnectionPage extends Component {
 
     onClickCancel() {
         changePage('connections');
+    }
+
+    render() {
+        var { host, port, database, user, password } = this.state;
+        return <div className="container add-connection-page">
+            <div className="page-header">
+                <h3>Add connection</h3>
+            </div>
+            <div className="form-group">
+                <div className="row">
+                    <div className="col-xs-8">
+                        <label>Host</label>
+                        <input type="text" name="host" value={host} onChange={this.bindValue.bind(this)} className="form-control" ref="host"/>
+                    </div>
+                    <div className="col-xs-4">
+                        <label>Port</label>
+                        <input type="number" name="port" value={port} onChange={this.bindValue.bind(this)} className="form-control"/>
+                    </div>
+                </div>
+            </div>
+            <div className="form-group">
+                <label>Database</label>
+                <input type="text" name="database" value={database} onChange={this.bindValue.bind(this)} className="form-control"/>
+            </div>
+            <div className="form-group">
+                <div className="row">
+                    <div className="col-xs-6">
+                        <label>User</label>
+                        <input type="text" name="user" value={user} onChange={this.bindValue.bind(this)} className="form-control"/>
+                    </div>
+                    <div className="col-xs-6">
+                        <label>Password</label>
+                        <input type="text" name="password" value={password} onChange={this.bindValue.bind(this)} className="form-control"/>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            <div className="form-group">
+                {this.renderCheckButton()}
+                {this.renderCheckStatus()}
+                <span className="text-danger">{this.state.connectionError}</span>
+            </div>
+            <hr/>
+            <button className="btn btn-primary disabled" onClick={this.onClickSave.bind(this)}>Save</button>
+            <button className="btn btn-default" onClick={this.onClickCancel.bind(this)}>Cancel</button>
+        </div>;
+    }
+
+    renderCheckButton() {
+        var disabled = ! this.isFormValid();
+        return <button className="btn btn-default" onClick={this.onClickCheckConnection.bind(this)} disabled={disabled}>
+            Check connection
+        </button>;
+    }
+
+    renderCheckStatus() {
+        var valid = this.state.connectionIsValid;
+        if (valid === null)
+            return '';
+        if (valid === true)
+            return <i className="fa fa-check"></i>;
+        if (valid === false)
+            return <i className="fa fa-ban"></i>;
+        return 'What';
     }
 }
