628c4c34f66d7c5cbb023e848b42e02da52e52f0
Hugo Mota <hugo.txt@gmail.com>
2017-03-12 13:43:40 -0300

    Convert connection actions in true store actions
    Making all actions true store actions will make it easy to
    manipulate state when needed. They also are logged by the store
    when debug mode is on.

--- src/connections/actions.js
+++ src/connections/actions.js
@@ -1,19 +1,20 @@
 import { ipcRenderer } from 'electron';
 import storage from 'electron-json-storage';
+import store from 'app/store';
 
 
-export function checkConnection(connection, callback) {
+export const checkConnection = store.action('checkConnection', (state, connection, callback) => {
     ipcRenderer.once('test-connection-response', (event, status) => {
         callback(status);
     });
     ipcRenderer.send('test-connection-request', connection);
-}
+});
 
-export function saveConnection(connection) {
+export const saveConnection = store.action('saveConnection', (state, connection) => {
     storage.get('connections', (error, connections) => {
         if (!Array.isArray(connections))
             connections = [];
         connections.push(connection);
         storage.set('connections', connections);
     });
-}
+});
